aa03ecb30f54d8a55566a2939b549fa99d26cac5
Yahia Mostafa <61708947+yahiamostafa@users.noreply.github.com>
2024-02-07 12:39:05 +0200

    Update vlsm_without_ui.py

--- vlsm_without_ui.py
+++ vlsm_without_ui.py
@@ -2,51 +2,35 @@ import math
 import ipaddress
 
 def calculate_vlsm(networkAddress , hosts):
-        # get network address and prefix
         networkAddress , prefix = networkAddress.split("/")
 
-        # network address as an ip Object
         networkIP = ipaddress.IPv4Address(networkAddress)
 
-        # get the four octets in the network address
         ip = list(map(int ,networkAddress.split(".")))
 
-        # get hosts 
         hosts = list(map(int ,hosts.split(",")))
-
-        # get the number of subnets required
         number_of_subents = len(hosts)
 
         # sort the number of hosts from the highest to the lowest 
         hosts.sort(reverse = True)
 
         # make a table to save values (the 4 well known ips)
+        # (Network_IP , First_Usable_IP , Last_Usable_IP, broadcastIP)
         table = []
 
         for host in hosts:
             
-            # get the number of bits required for this subnet
             number_of_bits = math.ceil(math.log2(host + 2))
             
-            # get the first Usable IP Address
             first_usable_ip_add = networkIP + 1
-
-            # get the broadcast ip Address
+                
             broadcast_ip_add = networkIP + 2 ** number_of_bits  - 1
-
-            # get the last Usable IP Address
             last_usable_ip_add = broadcast_ip_add - 1
 
-            # wasted IP addresses
             wasted_addresses = 2 ** number_of_bits - host
-
-            # create a list contains the 4 famous IPs
             IPs = [host , networkIP , first_usable_ip_add , last_usable_ip_add , broadcast_ip_add , 32 - number_of_bits , wasted_addresses]
 
-            # add the IPs to the table
             table.append(IPs)
-
-            # update the network IP
             networkIP += 2 ** number_of_bits
             
             print(IPs)
